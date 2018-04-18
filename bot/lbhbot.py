34aacf57520acef13ba1aca88c3938846b3cbe5a
Kevin Hallmark <khallmark@users.noreply.github.com>
2018-04-18 02:44:51 -0400

    Bot

--- bot/lbhbot.py
+++ bot/lbhbot.py
@@ -345,7 +345,7 @@ class LBHBot(commands.Bot):
         else:
             em.add_field(name="Male(%) / Female(%)", value="No Gender")
 
-        if pokemonObj.source == "game_master    ":
+        if pokemonObj.source == "game_master":
             em.add_field(name="Catch/Flee Rate", value="{}%/{}%".format(pokemonObj.baseCatchRate*100, pokemonObj.baseFleeRate*100))
 
         await self.say(embed=em)
